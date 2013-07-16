14a7c51666d4c7762d533aa651eb852ff6122e84
Xarlox <carfran1925@hotmail.com>
2013-07-16 08:52:29 -0500

    Conacatenar
    Concatenar cadenas utilizando metodos

---
+++ claces/Claces.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claces;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Claces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=10;
+        x=x+1;
+        int y=0;
+        System.out.println("x="+x);
+        suma(x,y);
+        System.out.println("conc"+y);
+        System.out.println("x="+x);
+    }
+    public static int suma (int y, int x){
+        y=1;
+        return y;
+           
+        
+    }
+}
